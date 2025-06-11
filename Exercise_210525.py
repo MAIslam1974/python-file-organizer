76dbe79822f3ce9754a399eeefe4de74616c22df
Aminul Islam <amin.bvtv@gmail.com>
2025-06-11 20:20:22 +0600

    Add files via upload

---
+++ Exercise_210525.py
@@ -0,0 +1,16 @@
+
+full_name = "Aminul Islam"
+age = 49
+gpa = 3.75
+is_student = True
+
+print(f"Hello {full_name}")
+print(f"you are {age} years old")
+print(f"your GPA is {gpa}")
+# print(f"Are you students?: {is_student}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT student")
+
+
